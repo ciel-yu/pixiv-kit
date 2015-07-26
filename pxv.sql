934b1980e474dd27c8537fca71020eeb2f7f08dc
ciel <ciel.blanc@gmail.com>
2015-07-26 15:15:19 +0800

    temp

--- pxv.sql
+++ pxv.sql
@@ -12,12 +12,25 @@ CREATE TABLE `user` (
 	`works`	INTEGER NOT NULL DEFAULT 0,
 	PRIMARY KEY(user_id)
 );
-CREATE TABLE `image` (
+CREATE TABLE `image_content` (
+	`image_id`	INTEGER NOT NULL,
+	`page_no`	INTEGER NOT NULL DEFAULT 0,
+	`url`	TEXT NOT NULL DEFAULT '',
+	`check_time`	INTEGER,
+	`path`	TEXT,
+	`size`	INTEGER NOT NULL DEFAULT -1,
+	`checksum`	TEXT,
+	PRIMARY KEY(image_id,page_no)
+);
+CREATE TABLE "image" (
 	`image_id`	INTEGER NOT NULL,
 	`user_id`	INTEGER NOT NULL DEFAULT -1,
 	`title`	TEXT NOT NULL DEFAULT '',
 	`caption`	TEXT NOT NULL DEFAULT '',
 	`tags`	TEXT NOT NULL DEFAULT '',
+	`page_count`	INTEGER NOT NULL DEFAULT 0,
+	`type`	TEXT NOT NULL DEFAULT '',
+	`status`	INTEGER NOT NULL DEFAULT -1,
 	PRIMARY KEY(image_id)
 );
 CREATE TABLE `bookmark` (
